b813971b102da953d1160ab9ab8cd7d0cede1c8b
NinaKoroleva <82880248+NinaKoroleva@users.noreply.github.com>
2024-11-19 21:52:59 +0300

    Update 2.py
    3

--- 2.py
+++ 2.py
@@ -7,19 +7,20 @@ def acounting(file:str) -> int:
 
 
 #1 Задача
-with open('2txt.txt', encoding='utf-8') as file:
-    cook_book = {}
-    for i in file:
-        recepie_name = i.strip()
-        ingredients_count = file.readline()
-        ingredients = []
-        for p in range(int(ingredients_count)):
-            recepie = file.readline().strip().split(' | ')
-            product, quantity, word = recepie
-            ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
-        file.readline()
-        cook_book[recepie_name] = ingredients
-
+def recipe_create():
+    with open('2txt.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for i in file:
+            recepie_name = i.strip()
+            ingredients_count = file.readline()
+            ingredients = []
+            for p in range(int(ingredients_count)):
+                recepie = file.readline().strip().split(' | ')
+                product, quantity, word = recepie
+                ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
+            file.readline()
+            cook_book[recepie_name] = ingredients
+    return cook_book
 
     
 #2 Задача            
@@ -30,7 +31,7 @@ def get_shop_list_by_dishes(person_count, dishes):
             for consist in cook_book[dish]:
                     if consist['product'] in result:
                     result[consist['product']]['quantity'] += consist['quantity'] * person_count
-                else:
+                    else:
                     result[consist['product']] = ({'measure': consist['measure'],'quantity': (consist['quantity'] * person_count)})
         else:
             print('Такого блюда нет в книге')
